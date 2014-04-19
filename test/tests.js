b1979bf629b899d64d1a157920d4701acd8e8d09
Mike Pennisi <mike@mikepennisi.com>
2014-04-18 22:03:11 -0400

    Generalize loader loading
    The state of the AMD loader must be refreshed before each test. Although
    the `context` configuration option serves this purpose for RequireJS, it
    is not implemented by other script loaders. Using a dedicated iFrame as
    the context for each test will allow for *all* AMD loaders to be
    re-initialized before every test.

--- test/tests.js
+++ test/tests.js
@@ -1,85 +1,103 @@
 suite('bower!!', function() {
 	'use strict';
 
-	// TODO: Re-implement to support AMD script loaders that do not support the
-	// `context` configuration option.
-	var createRequire = function(configuration) {
-		configuration.context = +(new Date());
-		return require.config(configuration);
-	};
+function testLoader(name, loaderName, url) {
+	suite(name, function() {
+		setup(function(done) {
+			var iframe = this.iframe = document.createElement('iframe');
+			iframe.setAttribute('style', 'display: none;');
+			document.body.appendChild(iframe);
+			var idoc = iframe.contentDocument;
+			var iwin = iframe.contentWindow;
+			var script = idoc.createElement('script');
+			script.setAttribute('src', '../node_modules/' + url + '.js');
 
-	setup(function() {
-		this.configuration = {
-			paths: {
-				bower: '../bower'
-			}
-		};
-		this.require = createRequire(this.configuration);
-	});
+			this.configuration = {
+				paths: {
+					bower: '../bower'
+				}
+			};
 
-	test('complex dependencies', function(done) {
-		this.require(['bower!layoutmanager'], function(Layout) {
-			assert.isFunction(Layout);
-			done();
-		});
-	});
-
-	test('named modules', function(done) {
-		this.require(['bower!jquery'], function($) {
-			assert.isFunction($);
-			done();
-		});
-	});
-
-	test('submodules', function(done) {
-		this.require(
-			['bower!when', 'bower!when/delay'],
-			function(when, whenDelay) {
-				assert.isFunction(when);
-				assert.isFunction(whenDelay);
-				assert.notEqual(when, whenDelay);
+			script.addEventListener('load', function() {
+				this.require = iwin[loaderName];
+				this.require.config(this.configuration);
 				done();
-			}
-		);
-	});
+			}.bind(this));
 
-	test('multiple invocations', function(done) {
-		var require = this.require;
-		require(['bower!when'], function() {
+			idoc.head.appendChild(script);
+		});
+
+		teardown(function() {
+			this.iframe.parentNode.removeChild(this.iframe);
+		});
+
+		test('complex dependencies', function(done) {
+			this.require(['bower!layoutmanager'], function(Layout) {
+				assert.isFunction(Layout);
+				done();
+			});
+		});
+
+		test('named modules', function(done) {
+			this.require(['bower!jquery'], function($) {
+				assert.isFunction($);
+				done();
+			});
+		});
+
+		test('submodules', function(done) {
+			this.require(
+				['bower!when', 'bower!when/delay'],
+				function(when, whenDelay) {
+					assert.isFunction(when);
+					assert.isFunction(whenDelay);
+					assert.notEqual(when, whenDelay);
+					done();
+				}
+			);
+		});
+
+		test('multiple invocations', function(done) {
+			var require = this.require;
 			require(['bower!when'], function() {
+				require(['bower!when'], function() {
+					done();
+				});
+			});
+		});
+
+		test('non-existent modules', function(done) {
+			this.require(['bower!non-existent'], function() {
+				done(new Error('Incorrectly invoked success callback'));
+			}, function(err) {
+				assert.ok(err);
+				done();
+			});
+		});
+
+		test('malformed bower.json files', function(done) {
+			this.require(['bower!bad-config'], function() {
+				done(new Error('Incorrectly invoked success callback'));
+			}, function(err) {
+				assert.ok(err);
+				done();
+			});
+		});
+
+		test('custom installation directory', function(done) {
+			this.configuration.paths.bower_components = 'custom-bower-dir';
+			this.require.config(this.configuration);
+			this.require(['bower!a'], function(A) {
+				assert.ok(A, 'Loads module');
+				assert.ok(A.B, 'Loads module dependencies');
+				assert.ok(A.B);
 				done();
 			});
 		});
 	});
 
-	test('non-existent modules', function(done) {
-		this.require = createRequire(this.configuration);
-		this.require(['bower!non-existent'], function() {
-			done(new Error('Incorrectly invoked success callback'));
-		}, function(err) {
-			assert.instanceOf(err, Error);
-			done();
-		});
-	});
+}
 
-	test('malformed bower.json files', function(done) {
-		this.require = createRequire(this.configuration);
-		this.require(['bower!bad-config'], function() {
-			done(new Error('Incorrectly invoked success callback'));
-		}, function(err) {
-			assert.instanceOf(err, Error);
-			done();
-		});
-	});
+testLoader('RequireJS', 'require', 'requirejs/require');
 
-	test('custom installation directory', function(done) {
-		this.configuration.paths.bower_components = 'custom-bower-dir';
-		this.require = createRequire(this.configuration);
-		this.require(['bower!a'], function(A) {
-			assert.ok(A, 'Loads module');
-			assert.ok(A.B, 'Loads module dependencies');
-			assert.ok(A.B);
-			done();
-		});
-	});
 });
